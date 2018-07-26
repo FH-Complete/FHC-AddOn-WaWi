979414e46c7d05f6b3fd04eaac4579ce25865341
Andreas Österreicher <oesi@technikum-wien.at>
2018-07-26 08:33:37 +0200

    Corrected DMS Upload File Permissions

--- vilesci/angebotUpload.php
+++ vilesci/angebotUpload.php
@@ -36,7 +36,7 @@ require_once dirname(__FILE__).'/../include/wawi_zuordnung.class.php';
 require_once dirname(__FILE__).'/../include/wawi_bestellung.class.php';
 require_once dirname(__FILE__).'/../include/wawi_kostenstelle.class.php';
 require_once dirname(__FILE__).'/../include/wawi_bestelldetail.class.php';
-require_once dirname(__FILE__).'/../include/wawi_aufteilung.class.php'; 
+require_once dirname(__FILE__).'/../include/wawi_aufteilung.class.php';
 require_once dirname(__FILE__).'/../include/wawi_bestellstatus.class.php';
 require_once dirname(__FILE__).'/../include/wawi_zahlungstyp.class.php';
 require_once(dirname(__FILE__).'/../../../include/tags.class.php');
@@ -46,36 +46,38 @@ require_once(dirname(__FILE__).'/../../../include/dms.class.php');
 $user=get_uid();
 $ausgabemsg='';
 
-$berechtigung_kurzbz='wawi/bestellung'; 
+$berechtigung_kurzbz='wawi/bestellung';
 $rechte = new benutzerberechtigung();
 $rechte->getBerechtigungen($user);
-$kst=new wawi_kostenstelle(); 
-$kst->loadArray($rechte->getKostenstelle($berechtigung_kurzbz),'bezeichnung'); 
-//$bestellung_kategorie=new wawi_bestellung_kategorie(); 
-//$bestellung_kategorie->getAll(true); 
+$kst=new wawi_kostenstelle();
+$kst->loadArray($rechte->getKostenstelle($berechtigung_kurzbz),'bezeichnung');
+//$bestellung_kategorie=new wawi_bestellung_kategorie();
+//$bestellung_kategorie->getAll(true);
 
-$projekt = new projekt(); 
+$projekt = new projekt();
 $projekt->getProjekteMitarbeiter($user);
-$projektZugeordnet = false; 
+$projektZugeordnet = false;
 
 // wawi dms kategorie
 $kategorie_kurzbz = 'angebote';
 
 // Abfrage ob dem user ein oder mehrere Projekte zugeordnet sind
 if(count($projekt->result) > 0)
-	$projektZugeordnet = true; 
+	$projektZugeordnet = true;
 
 
 //var_dump($_FILES);
-$ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION); 
+$ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 $filename = uniqid();
-$filename.=".".$ext; 
+$filename.=".".$ext;
 $uploadfile = DMS_PATH.$filename;
-if(move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) 
+if(move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile))
 {
 	echo $_FILES['file']['name']. " OK";
 
 	$dms = new dms();
+	if(!$dms->setPermission($uploadfile))
+		echo $dms->errormsg;
 
 	$dms->version='0';
 	$dms->kategorie_kurzbz=$kategorie_kurzbz;
